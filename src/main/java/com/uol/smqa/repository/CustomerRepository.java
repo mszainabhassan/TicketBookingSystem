06f8e1c87b90f1634a6d1d1faf96568c1be3bbc3
zainab.jawaid <zainab.jawaid@aamanto.com>
2023-11-28 11:59:23 +0000

    completed customer repository interface

--- src/main/java/com/uol/smqa/repository/CustomerRepository.java
+++ src/main/java/com/uol/smqa/repository/CustomerRepository.java
@@ -1,5 +1,10 @@
 package com.uol.smqa.repository;
+import com.uol.smqa.model.Customer;
 
-public interface CustomerRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Integer>{
 
 }
