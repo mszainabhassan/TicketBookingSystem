a77918e03a8032dcad8cede4902379520e58284a
zainab.jawaid <zainab.jawaid@aamanto.com>
2023-11-29 16:49:30 +0000

    created TBSFMD-125 Admin approve / reject the event api

--- src/main/java/com/uol/smqa/controller/AdminController.java
+++ src/main/java/com/uol/smqa/controller/AdminController.java
@@ -1,5 +1,28 @@
 package com.uol.smqa.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.uol.smqa.service.EventService;
+
+import jakarta.websocket.server.PathParam;
+
+@RestController
+@RequestMapping("/admin")
 public class AdminController {
+
+	@Autowired
+	private EventService eventService;
+
+	@PutMapping("/change_event_status")
+	public String ChangeEventStatus(@RequestParam(name = "eventId") Integer eventId,
+			@RequestParam(name = "status") Boolean status) {
+
+		return this.eventService.ChangeEventStatus(eventId, status);
+
+	}
 
 }
--- src/main/java/com/uol/smqa/service/EventService.java
+++ src/main/java/com/uol/smqa/service/EventService.java
@@ -1,5 +1,27 @@
 package com.uol.smqa.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.uol.smqa.model.Event;
+import com.uol.smqa.repository.EventRepository;
+
+@Service
 public class EventService {
+
+	@Autowired
+	private EventRepository eventRepository;
+
+	public String ChangeEventStatus(int eventId, Boolean status) {
+		Event event = this.eventRepository.findById(eventId);
+
+		if (event != null) {
+			event.setStatus(status);
+			this.eventRepository.save(event);
+			return "Event Status Changed!";
+		} else {
+			return "Event Id not present!";
+		}
+	}
 
 }
