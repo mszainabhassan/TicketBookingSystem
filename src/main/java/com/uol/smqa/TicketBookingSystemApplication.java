f385ae5f60e9d5d3f7a51da5f387778db91e593a
Ogunboyejo Olatunde J <josiaholatunde17@gmail.com>
2023-11-30 17:29:36 +0100

    fix: Added implementation to save tickets

--- src/main/java/com/uol/smqa/TicketBookingSystemApplication.java
+++ src/main/java/com/uol/smqa/TicketBookingSystemApplication.java
@@ -1,13 +1,39 @@
 package com.uol.smqa;
 
+import com.uol.smqa.model.Users;
+import com.uol.smqa.repository.UsersRepository;
+import com.uol.smqa.service.UsersService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Optional;
 
 @SpringBootApplication
-public class TicketBookingSystemApplication {
+@RequiredArgsConstructor
+public class TicketBookingSystemApplication implements ApplicationRunner {
+	private final UsersService usersService;
+	private final UsersRepository usersRepository;
+	private final PasswordEncoder passwordEncoder;
 
 	public static void main(String[] args) {
 		SpringApplication.run(TicketBookingSystemApplication.class, args);
 	}
 
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		Optional<Users> adminUser = usersService.findByEmail("admin@tbs.com");
+		if (!adminUser.isPresent()) {
+			Users admin = new Users();
+			admin.setUsername("admin@tbs.com");
+			admin.setPassword(passwordEncoder.encode("password"));
+
+			usersRepository.save(admin);
+		}
+
+	}
 }
