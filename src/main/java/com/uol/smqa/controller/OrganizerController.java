276a402f70244b308710866b5ba49a43f5102496
sridivya1 <sridivya4723@gmail.com>
2023-12-02 16:08:19 +0000

    eventanalytics

--- src/main/java/com/uol/smqa/controller/OrganizerController.java
+++ src/main/java/com/uol/smqa/controller/OrganizerController.java
@@ -3,13 +3,13 @@ package com.uol.smqa.controller;
 import com.uol.smqa.service.EventService;
 import com.uol.smqa.service.EventTypeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import com.uol.smqa.model.CustomerBookEvent;
 import com.uol.smqa.model.Event;
 import com.uol.smqa.model.EventType;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/organizer")
@@ -30,5 +30,22 @@ public class OrganizerController {
         // Set EventType in Event and Save Event
         event.setEventType(savedEventType);
         return eventService.createEvent(event);
+    }
+
+    @GetMapping("/eventAnalytics/{eventId}")
+    public String getEventAnalytics(@PathVariable Integer eventId) {
+        try {
+            // Get the event
+            Event event = eventService.getEventById(eventId);
+
+            // Get the number of users booked for the event
+            List<CustomerBookEvent> bookedCustomers = event.getBookedCustomers();
+            int numberOfBookedUsers = bookedCustomers.size();
+
+            // Return the analytics as a string (you can format it as needed)
+            return "Event Analytics for Event ID " + eventId + ": Number of Booked Users - " + numberOfBookedUsers;
+        } catch (Exception e) {
+            return "Error retrieving event analytics: " + e.getMessage();
+        }
     }
 }
--- src/main/java/com/uol/smqa/service/EventService.java
+++ src/main/java/com/uol/smqa/service/EventService.java
@@ -1,6 +1,7 @@
 package com.uol.smqa.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,14 +19,14 @@ public class EventService {
     private EventRepository eventRepository;
     
     @Autowired
-    private CustomerRepository customerRepository; // Fix the typo here
+    private CustomerRepository customerRepository;
     
     @Autowired
     private CustomerBookEventRepository customerBookEventRepository;
 
     public Event bookEvent(Integer eventId, Integer customerId) {
-        Event event = eventRepository.findById(eventId).orElseThrow();
-        Customer customer = customerRepository.findById(customerId).orElseThrow(); // Fix the typo here
+        Event event = getEventById(eventId);
+        Customer customer = customerRepository.findById(customerId).orElseThrow();
 
         CustomerBookEvent booking = new CustomerBookEvent();
         booking.setCustomer(customer);
@@ -43,5 +44,18 @@ public class EventService {
 
     public Event createEvent(Event event) {
         return eventRepository.save(event);
+    }
+
+    public int getNumberOfBookedUsers(Integer eventId) {
+        Event event = getEventById(eventId);
+
+        // Get the number of booked customers for the event
+        List<CustomerBookEvent> bookedCustomers = event.getBookedCustomers();
+        return bookedCustomers.size();
+    }
+
+    public Event getEventById(Integer eventId) {
+        return eventRepository.findById(eventId)
+                .orElseThrow(() -> new RuntimeException("Event not found with ID: " + eventId));
     }
 }
