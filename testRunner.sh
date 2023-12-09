faa9fdfbff6e752cd662e9f4f2e190521466d2a9
Ogunboyejo Olatunde J <josiaholatunde17@gmail.com>
2023-12-09 15:20:28 +0100

    fix: Added method to handle duplicate customer email registration

--- testRunner.sh
+++ testRunner.sh
@@ -14,4 +14,4 @@ javac -Xlint:deprecation -Xlint:unchecked -d classes -cp "junit-platform-console
 java -cp "classes:junit-platform-console-standalone-1.10.1.jar:$classpath" org.junit.platform.console.ConsoleLauncher --class-path=classes --scan-class-path
 
 # Execution command for Jacoco report generation
-java -javaagent:jacoco-0.8.11/lib/jacocoagent.jar -jar jacoco-0.8.11/lib/jacococli.jar report jacoco.exec --classfiles target/classes --sourcefiles src/main/java/com/uol/smqa --html report
+java -javaagent:jacoco-0.8.11/lib/jacocoagent.jar -jar jacoco-0.8.11/lib/jacococli.jar report jacoco.exec --classfiles ./classes --sourcefiles src/main/java --html report
