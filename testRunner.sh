e43e69374fbf47c3fb8f3b40a4e9e1fe4bcd3d7f
Ogunboyejo Olatunde J <josiaholatunde17@gmail.com>
2023-12-10 13:36:23 +0100

    fix: Removed unwanted files from the git history

--- testRunner.sh
+++ testRunner.sh
@@ -11,12 +11,5 @@ classpath="$classpath:src/test/resources"
 echo "Compilation command using the generated classpath"
 javac -Xlint:deprecation -Xlint:unchecked -d classes -cp "junit-platform-console-standalone-1.10.1.jar:$classpath:classes" -g $(find src/main/java -name "*.java") $(find src/test/java -name "*.java")
 
-# Execution command for JUnit tests
-echo "Execution command for JUnit tests"
-java -cp "classes:junit-platform-console-standalone-1.10.1.jar:$classpath" org.junit.platform.console.ConsoleLauncher --class-path=classes --scan-class-path
 
-# Execution command for Jacoco report generation
-echo "Execution command for Jacoco report generation"
-java -javaagent:jacoco-0.8.8/lib/jacocoagent.jar -jar jacoco-0.8.8/lib/jacococli.jar report jacoco.exec --classfiles classes --sourcefiles src/main/java --html report
-java -javaagent:jacoco-0.8.8/lib/jacocoagent.jar=includes=com/uol/smqa,* -jar jacoco-0.8.8/lib/jacococli.jar report jacoco.exec --classfiles classes --sourcefiles src/main/java --html report
-
+java -javaagent:jacoco-0.8.8/lib/jacocoagent.jar=destfile=target/coverage.exec -cp "classes:junit-platform-console-standalone-1.10.1.jar:$classpath" org.junit.runner.JUnitCore $(find src/test/java -name "*.java" -exec basename {} \; | sed 's/\.java$//' | tr '\n' ' ')
